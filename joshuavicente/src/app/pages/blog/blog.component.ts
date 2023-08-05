f7b5a3683138e3e9fd17b06ebabe2884eb5245b4
Joshua Vicente <owentatoy@gmail.com>
2023-08-05 19:22:11 +0800

    feat; Generated blog component

--- joshuavicente/src/app/pages/blog/blog.component.ts
+++ joshuavicente/src/app/pages/blog/blog.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogService, BlogArticle } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss']
 })
-export class BlogComponent {
+export class BlogComponent implements OnInit {
+  articles: BlogArticle[] = [];
 
+  constructor(private blogService: BlogService) { }
+
+  ngOnInit(): void {
+    this.articles = this.blogService.getAllArticles();
+    console.log(this.articles)
+  }
 }
