5ac1b5c7f7d121314ed96dbecce572c229b5228d
Joshua Vicente <owentatoy@gmail.com>
2023-08-09 18:36:08 +0800

    feat: Created model component which contains mock data

---
+++ joshuavicente/src/app/models/job-data.model.ts
@@ -0,0 +1,23 @@
+export const jobs = [
+  {
+    date: '2022 — Present',
+    link: 'https://www.nalagenetics.com',
+    title: 'Software Engineer (Front-End) · Nalagenetics Pte Ltd',
+    description: 'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.',
+    skills: ['Angular', 'SCSS', 'JavaScript', 'TypeScript']
+  },
+  {
+    date: '2022 — Present',
+    link: 'https://www.nalagenetics.com',
+    title: 'Software Engineer (Front-End) · Nalagenetics Pte Ltd',
+    description: 'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.',
+    skills: ['Angular', 'SCSS', 'JavaScript', 'TypeScript']
+  },
+  {
+    date: '2022 — Present',
+    link: 'https://www.nalagenetics.com',
+    title: 'Software Engineer (Front-End) · Nalagenetics Pte Ltd',
+    description: 'Deliver high-quality, robust production code for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and mentorship.',
+    skills: ['Angular', 'SCSS', 'JavaScript', 'TypeScript']
+  },
+];
