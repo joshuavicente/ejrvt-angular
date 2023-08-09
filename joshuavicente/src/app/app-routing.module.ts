0c945d41e33f5bfb5bb033c29b1b4c09094bc573
Joshua Vicente <owentatoy@gmail.com>
2023-08-09 18:35:21 +0800

    feat: Updated module.ts and routing to accommodate new and removed components

--- joshuavicente/src/app/app-routing.module.ts
+++ joshuavicente/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
 import { BlogPostComponent } from './pages/blog-post/blog-post.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PdfViewerComponent } from './pages/pdf-viewer/pdf-viewer.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'resume.pdf', component: PdfViewerComponent, data: { pdfSrc: '/assets/files/Joshua_Vicente_Resume.pdf' } },
   { path: 'blog', component: BlogComponent },
   { path: 'blog/:tag', component: BlogPostComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', scrollPositionRestoration: 'disabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- joshuavicente/src/app/app.module.ts
+++ joshuavicente/src/app/app.module.ts
@@ -3,21 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { AboutComponent } from './pages/about/about.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { BlogPostComponent } from './pages/blog-post/blog-post.component';
+import { PdfViewerComponent } from './pages/pdf-viewer/pdf-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    AboutComponent,
     BlogComponent,
-    BlogPostComponent
+    BlogPostComponent,
+    PdfViewerComponent
   ],
   imports: [
     BrowserModule,
