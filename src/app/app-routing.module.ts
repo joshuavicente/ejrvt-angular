e3ba7413f9b9d43c9bc804015f9188d1c7314b18
Joshua Vicente <owentatoy@gmail.com>
2023-08-12 14:54:19 +0800

    chore: Removed unused components and updated routing and imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BlogPostComponent } from './pages/blog-post/blog-post.component';
-import { BlogComponent } from './pages/blog/blog.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PdfViewerComponent } from './pages/pdf-viewer/pdf-viewer.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'resume.pdf', component: PdfViewerComponent, data: { pdfSrc: '/assets/files/Joshua_Vicente_Resume.pdf' } },
-  { path: 'blog', component: BlogComponent },
-  { path: 'blog/:tag', component: BlogPostComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BlogComponent } from './pages/blog/blog.component';
-import { BlogPostComponent } from './pages/blog-post/blog-post.component';
 import { PdfViewerComponent } from './pages/pdf-viewer/pdf-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BlogComponent,
-    BlogPostComponent,
     PdfViewerComponent
   ],
   imports: [
